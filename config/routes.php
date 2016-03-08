35b304c88b1136822261c31591a7400498edc1fb
César Juárez Díaz <cesar.juarez@apselom.com>
2016-03-08 12:59:32 -0500

    Making custom urls

--- config/routes.php
+++ config/routes.php
@@ -51,6 +51,8 @@ Router::scope('/', function (RouteBuilder $routes) {
   $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
   $routes->connect('/begin', ['controller' => 'Business', 'action' => 'add']);
+  $routes->connect('/login', ['controller' => 'Users', 'action' => 'login']);
+  $routes->connect('/logout', ['controller' => 'Users', 'action' => 'logout']);
 
 
   /**
